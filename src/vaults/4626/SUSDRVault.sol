aafb927ff1ff4aaa4afe73d03778a6695c2cf60f
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2025-09-17 18:18:21 +0400

    fix: respond to PR comments

--- src/vaults/4626/SUSDRVault.sol
+++ src/vaults/4626/SUSDRVault.sol
@@ -4,18 +4,24 @@ pragma solidity ^0.8.26;
 import {AccessControl} from '@openzeppelin/contracts/access/AccessControl.sol';
 import {ERC20} from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import {SafeTransferLib} from 'solady/utils/SafeTransferLib.sol';
 import {ERC4626} from '@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol';
 import {Pausable} from '@openzeppelin/contracts/utils/Pausable.sol';
 import {ReentrancyGuard} from '@openzeppelin/contracts/utils/ReentrancyGuard.sol';
+import {ISUSDRVaultEventsAndErrors} from '../../interfaces/vaults/4626/ISUSDRVaultEventsAndErrors.sol';
 
 // Note: non-upgradeable version
 // Note: We could have some admin actions
 
 /// @title sUSDRVault — ERC-4626: deposit USDR → mint sUSDR; external asset inflows lift PPS
-contract SUSDRVault is ERC20, ERC4626, AccessControl, Pausable, ReentrancyGuard {
+contract SUSDRVault is ERC20, ERC4626, AccessControl, Pausable, ReentrancyGuard, ISUSDRVaultEventsAndErrors {
+  using SafeTransferLib for IERC20;
+
   bytes32 public constant PAUSER_ROLE = keccak256('PAUSER_ROLE');
 
   constructor(IERC20 usdr, address admin, address pauser) ERC20('Staked USDR', 'sUSDR') ERC4626(usdr) {
+    if (admin == address(0)) revert AdminCannotBeZeroAddress();
+    if (pauser == address(0)) revert PauserCannotBeZeroAddress();
     _grantRole(DEFAULT_ADMIN_ROLE, admin);
     _grantRole(PAUSER_ROLE, pauser);
   }
@@ -57,4 +63,17 @@ contract SUSDRVault is ERC20, ERC4626, AccessControl, Pausable, ReentrancyGuard 
   function decimals() public view override(ERC20, ERC4626) returns (uint8) {
     return super.decimals();
   }
+
+  // Recover non-asset ERC20 tokens
+  // This is used to recover tokens that are sent to the vault by mistake  
+  function recoverNonAssetERC20(address token, address to, uint256 amount) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    if(token == asset()) revert TokenCannotBeUSDR();
+    if (token == address(0)) revert TokenCannotBeZeroAddress();
+    if (to == address(0)) revert ToCannotBeZeroAddress();
+    if (amount == 0) revert AmountCannotBeZero();
+    SafeTransferLib.safeTransfer(token, to, amount);
+  }
+
+  // Todo // Review
+  // Decide if we want to accept eth, withdraw eth etc or not.
 }
